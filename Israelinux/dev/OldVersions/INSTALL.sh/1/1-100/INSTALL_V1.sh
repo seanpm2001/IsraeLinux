9bf013714ff7a1cd3f6f08fbc8f2f019fbf57098
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-08 15:54:41 -0800

    INSTALL_V1.sh
    An archive of the first version of the installation script file for this project.

---
+++ Israelinux/dev/OldVersions/INSTALL.sh/1/1-100/INSTALL_V1.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Start of script
+# Installing IsraeLinux
+
+sudo install plasma-desktop
+sudo install okular
+sudo install kate
+sudo install vlc
+sudo install dolphin
+sudo install python3
+# More installations coming soon
+
+# File info
+# File type: Shell script file (*.sh)
+# File version: 1 (2022, Tuesday, February 8th at 3:54 pm) 
+# Line count (including blank lines and compiler line): 18
+# End of script
