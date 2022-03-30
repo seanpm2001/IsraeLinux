7194edfeaa0ab91804e7d63129c2c01e9a32efba
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-03-29 17:51:07 -0700

    neofetch.israelinux.sh [V1]
    Version 1 of the neofetch configuration file for this project.

---
+++ neofetch.israelinux.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Start of script
+# The Israeli flag in Neofetch
+function neofetch_IsraeLinux {
+  neofetch -ascii_distro centos --ascii_colors  0 0 0 0 F F  # What is the color code for blue in Neofetch?
+}
+# File info
+# File type: Shell script file (*.sh)
+# File version: 1 (2022, Tuesday, March 29th at 5:51 pm)
+# Line count (including blank lines and compiler line): 12
+# End of script
